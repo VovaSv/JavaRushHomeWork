98917127d7cf4c272e8348ee8feaebe9b72b0eba
vladimis <vovasvidi@gmail.com>
2016-09-13 11:42:58 +0300

    Added new file MainThread

--- src/com/javarush/test/Thread_Thinking_Java/MainThread.java
+++ src/com/javarush/test/Thread_Thinking_Java/MainThread.java
@@ -29,8 +29,9 @@ public class MainThread
 
     public static void main(String[] args)
     {
-        LiftOff launch = new LiftOff();
-        launch.run();
+        Thread t = new Thread(new LiftOff());
+        t.start();
+        System.out.println("Waiting for LiftOff");
     }
 
 }
